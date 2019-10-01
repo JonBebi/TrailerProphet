df9dc062395bd83021cda695aa245796b8bc1f6d
Xristos <xristos.kvtsvros@gmail.com>
2019-10-01 16:11:26 -0400

    crated functions

--- twitter.py
+++ twitter.py
@@ -1,30 +1,47 @@
 import twint
 import nest_asyncio
+import pandas as pd
 
 ## Apply nest_asyncio so the event loop in twint doesn't conflict with jupyter notebook
 nest_asyncio.apply()
 
-## Instantiate twint
-c = twint.Config()
-
-## Collect tweets from "Hustlers" movie and place them in a dataframe
-c.Search = "Hustlers movie"
-c.Pandas = True
-twint.run.Search(c)
-hustlers_df = twint.storage.panda.hustlers_df
-hustlers_df.head()
-
-## Collect tweets from "Downton Abbey" movie and place them in a dataframe
-c.Search = "Downton Abbey"
-c.Pandas = True
-twint.run.Search(c)
-abbey_df = twint.storage.panda.abbey_df
-
+## Analyze the sentiment of all the tweets
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 analyzer = SentimentIntensityAnalyzer()
+
+## Function takes in the pandas series that holds all of the tweets and hold them in a list
 def tweet_analysis(series):
+    score_list = []
     for tweet in series:
         vs = analyzer.polarity_scores(tweet)
-        print(f'{tweet}: {str(vs)}')
+        score_list.append(vs['compound'])
+    return score_list
 
-tweet_analysis(Tweets_df['tweet'])
+## Function that takes in a movie and a release date, then scrapes twitter for 3000 tweets before the date
+## analyzes all the tweets and returns the mean
+
+def what_twitter_thinks(movie, date):
+    ## set up the twitter search
+    c = twint.Config()
+    c.Search = movie
+    c.Until = date
+    c.Pandas = True
+    twint.run.Search(c)
+
+    ## create the dataframe
+    movie_df = twint.storage.panda.Tweets_df
+    movie_df['score'] = pd.Series(tweet_analysis(movie_df['tweet']))
+
+    return movie_df['score'].mean()
+
+## Analyze Hustlers movie
+hustlers = "Hustlers movie"
+hustlers_date = '2019-09-12'
+
+## Analyze Downton Abbey
+downton_abbey = "Downton Abbey movie"
+da_date = "2019-09-19"
+
+## Analyze Abominable
+abominable = "Abominable movie"
+abominable_date = "2019-09-26"
